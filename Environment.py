2965107ae5d97ee1de46ceb0cf0755ee5ceef34e
jd7jez <jd@issaquahsports.com>
2024-10-11 18:31:07 -0700

    Made plant hyperparameters more easily changable. Made environment process plants in parallel.

--- Environment.py
+++ Environment.py
@@ -5,6 +5,7 @@ from Plant import Plant
 import random
 import matplotlib.pyplot as plt
 import sys
+from concurrent.futures import ThreadPoolExecutor
 
 class Environment:
     def __init__(self, width=1400, height=900):
@@ -14,7 +15,8 @@ class Environment:
         # OLD SCR: 0.5-2.0
         self.BACKGROUND = (255, 255, 255)
         self.sun = Sun(self.WIDTH, self.HEIGHT, self.WIDTH, 10)
-        self.plants = [Plant(random.randint(0, self.WIDTH), random.randint(0, self.HEIGHT), 50.0, 0,
+        self.plant_squares = [[0 for _ in range((self.WIDTH // 50) + 1)] for _ in range((self.HEIGHT // 50) + 1)]
+        self.plants = [Plant(random.randint(0, self.WIDTH), random.randint(0, self.HEIGHT), 0, 50.0,
                              energy_change_rate=(random.random() *4.75) + 0.25, size_change_rate=(random.random() * 3.5) + 0.5)
                        for _ in range(50)]
 
@@ -27,31 +29,44 @@ class Environment:
         self.scrs = []
         self.lifespans = []
 
+    def process_plant(self, plant):
+        x_i = plant.x // 50
+        y_i = plant.y // 50
+        code = plant.pass_time(self.sun, self.plant_squares[y_i][x_i])
+        birth = None
+        if code == 1:
+            birth = plant.birth(self.WIDTH, self.HEIGHT, self.sun.day)
+        if code != -1:
+            plant.draw(self.screen)
+        return (plant, code, birth)
+
     def simulate(self):
         running = True
-        while running and self.sun.day < 5:
+        while running and self.sun.day < 15:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            self.plant_squares = [[0 for _ in range((self.WIDTH // 50) + 1)] for _ in range((self.HEIGHT // 50) + 1)]
+            for plant in self.plants:
+                x_i = plant.x // 50
+                y_i = plant.y // 50
+                self.plant_squares[y_i][x_i] += 1
                     
             self.screen.fill(self.BACKGROUND)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
             self.sun.draw(self.screen)
+
+            with ThreadPoolExecutor() as executor:
+                plant_results = list(executor.map(self.process_plant, self.plants))
 
             dead = []
             birthed = []
-            for i, plant in enumerate(self.plants):
-                code = plant.pass_time(self.sun)
+            for i, (plant, code, birth) in enumerate(plant_results):
                 if code == 1:
-                    birthed.append(plant.birth(self.WIDTH, self.HEIGHT, self.sun.day))
-                if code != -1:
-                    plant.draw(self.screen)
-                else:
+                    birthed.append(birth)
+                if code == -1:
                     dead.append(i)
 
             for i in dead[::-1]:
@@ -68,9 +83,8 @@ class Environment:
             self.sun.move()
 
             self.clock.tick(70)
-        print("Quitting pygame")
+
         pygame.quit()
-        print("Quit pygame")
         
         for plant in self.plants:
             self.ecrs.append(plant.energy_change_rate)
--- Plant.py
+++ Plant.py
@@ -2,6 +2,17 @@ import pygame
 import random
 
 class Plant:
+
+    health_threshold = 50
+    decay_threshold = 40
+    min_size = 5
+    max_vitality = 100
+    grow_threshold = 100
+    reproduce_threshold = 400
+    reproduce_chance = 0.5
+    birth_cooldown_max = 10
+    density_limit = 15
+
     def __init__(self, x, y, birth_date, initial_energy=50.0, energy_change_rate=1.0, size_change_rate=1.0):
         self.x = x
         self.y = y
@@ -9,32 +20,32 @@ class Plant:
         self.energy = initial_energy
         self.energy_change_rate = energy_change_rate
         self.size_change_rate = size_change_rate
-        self.size = 10.0
+        self.size = 5.0
         self.color1 = int(255 * (self.energy_change_rate / 5.0))
         self.color3 = int(255 * (self.size_change_rate / 4.0))
         self.vitality = 100
-        self.birth_cooldown = 10
+        self.birth_cooldown = self.birth_cooldown_max
 
     def draw(self, surface):
         green_score = int(255 * (self.vitality / 100)) if self.vitality > 0 else 0
         pygame.draw.circle(surface, (self.color1, green_score, self.color3), (self.x, self.y), self.size)
 
-    def pass_time(self, sun):
+    def pass_time(self, sun, density):
         # Decrease the energy based on the size of the plant and then increase based on sunlight
         # Both of these changes will be scaled based on the energy change rate
         base_energy_lost = self.size // 10 if self.size // 10 > 0 else (1 if self.energy > 0 else 0)
         self.energy -= base_energy_lost * self.energy_change_rate
         base_energy_gained = sun.give_energy(self.x)
         self.energy += base_energy_gained * self.energy_change_rate
-        if self.energy >= 10:
-            if self.vitality < 100:
+        if self.energy >= self.health_threshold:
+            if self.vitality < self.max_vitality:
                 self.vitality += 1
-            if self.energy > 50:
-                if self.energy > 400:
+            if self.energy > self.grow_threshold:
+                if self.energy > self.reproduce_threshold:
                     reproduce_odds = random.random()
-                    if reproduce_odds < 0.5:
+                    if reproduce_odds < self.reproduce_chance and density < self.density_limit:
                         if self.birth_cooldown == 0:
-                            self.birth_cooldown = 10
+                            self.birth_cooldown = self.birth_cooldown_max
                             return 1
                         else:
                             self.birth_cooldown -= 1
@@ -42,8 +53,8 @@ class Plant:
                         self.size += self.size_change_rate
                 else:
                     self.size += self.size_change_rate
-        elif self.energy < 10:
-            if self.size > 5:
+        elif self.energy < self.decay_threshold:
+            if self.size > self.min_size:
                 self.size -= self.size_change_rate
             else:
                 self.vitality -= 1
